dc7d3a684944e0700d7c0383c9be92812d9566ff
Robin Ren <robin.ren@morningstar.com>
2018-08-13 15:05:00 +0800

    update

--- src/main/java/sports/HistoryAddIn.java
+++ src/main/java/sports/HistoryAddIn.java
@@ -16,91 +16,96 @@ import org.xml.sax.SAXException;
 
 public class HistoryAddIn {
 
-	public static String sendGet() {
+    public static String sendGet() {
 
-		String spec = "http://www.lottery.gov.cn/historykj/history.jspx?_ltype=dlt";
+        String spec = "http://www.lottery.gov.cn/historykj/history.jspx?_ltype=dlt";
 
-		HttpURLConnection httpURLConnection = null;
-		InputStream inputStream = null;
-		BufferedReader bufferedReader = null;
-		String result = null;// 返回结果字符串
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+        String result = null;// 返回结果字符串
 
-		try {
-			// 创建远程url连接对象
-			URL url = new URL(spec);
-			// 通过远程url连接对象打开一个连接，强转成httpURLConnection类
-			httpURLConnection = (HttpURLConnection) url.openConnection();
-			// 设置连接方式：get
-			httpURLConnection.setRequestMethod("GET");
-			// 设置连接主机服务器的超时时间：15000毫秒
-			httpURLConnection.setConnectTimeout(3000);
-			// 设置读取远程返回的数据时间：60000毫秒
-			httpURLConnection.setReadTimeout(6000);
-			// 发送请求
-			httpURLConnection.connect();
-			// 通过connection连接，获取输入流
-			inputStream = httpURLConnection.getInputStream();
-			// 封装输入流inputStream，并指定字符集
-			bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-			// 存放数据
-			StringBuffer stringBuffer = new StringBuffer();
-			String temp = null;
-			while ((temp = bufferedReader.readLine()) != null) {
-				stringBuffer.append(temp);
-				stringBuffer.append("\r\n");
-			}
-			result = stringBuffer.toString();
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(spec);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            httpURLConnection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            httpURLConnection.setConnectTimeout(3000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            httpURLConnection.setReadTimeout(6000);
+            // 发送请求
+            httpURLConnection.connect();
+            // 通过connection连接，获取输入流
+            inputStream = httpURLConnection.getInputStream();
+            // 封装输入流inputStream，并指定字符集
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            // 存放数据
+            StringBuffer stringBuffer = new StringBuffer();
+            String temp = null;
+            while ((temp = bufferedReader.readLine()) != null) {
+                stringBuffer.append(temp);
+                stringBuffer.append("\r\n");
+            }
+            result = stringBuffer.toString();
 
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// 关闭资源
-			if (null != bufferedReader) {
-				try {
-					bufferedReader.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != bufferedReader) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
 
-			if (null != inputStream) {
-				try {
-					inputStream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+            if (null != inputStream) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
 
-			httpURLConnection.disconnect();
-		}
-		return result;
-	}
+            httpURLConnection.disconnect();
+        }
+        return result;
+    }
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-		String response = sendGet();
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        String response = sendGet();
 //		System.out.println(response);
-		
+
 //		Pattern pet = Pattern.compile("<tbody>(.|\\n)*</tbody>");
-		Pattern pet = Pattern.compile("<tbody>(.|\\n)*<\\/tbody>");
+//		Pattern pet = Pattern.compile("\\b\\w{3}\\b");
+        Pattern pet = Pattern.compile("<tbody>(.*\\|\\n*)*</tbody>");
         Matcher match = pet.matcher(response);
 
         System.out.println(match.find());
-        
-		// StringReader sr = new StringReader(response);
-		// InputSource is = new InputSource(sr);
-		// DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		// DocumentBuilder builder = factory.newDocumentBuilder();
-		// Document doc = builder.parse(is);
-		//
-		// String nodeValue = doc.getNodeValue();
-		
 
-	}
+        System.out.println(match.group(0));
+        System.out.println(match.group(1));
+
+
+        // StringReader sr = new StringReader(response);
+        // InputSource is = new InputSource(sr);
+        // DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        // DocumentBuilder builder = factory.newDocumentBuilder();
+        // Document doc = builder.parse(is);
+        //
+        // String nodeValue = doc.getNodeValue();
+
+
+    }
 
 }
--- src/main/java/sports/Sports.java
+++ src/main/java/sports/Sports.java
@@ -8,177 +8,173 @@ import groovy.util.Node;
 
 public class Sports {
 
-	static Common common = new Common();
+    static Common common = new Common();
 
-	// 每一注 相同数分析
-	static int[] redSame = new int[6];
-	static int[] blueSame = new int[3];
-	static int[] allSame = new int[8];
+    // 每一注 相同数分析
+    static int[] redSame = new int[6];
+    static int[] blueSame = new int[3];
+    static int[] allSame = new int[8];
 
-	static int cal = 0;
+    static int cal = 0;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("\n\nSports:" + common.getCurrentDate());
-		get5_sports();
+        System.out.println("\n\nSports:" + common.getCurrentDate());
+        get5_sports();
 
-	}
+    }
 
-	/**
-	 * 生成5注
-	 * 
-	 * @param sameSize
-	 *            最大相同数
-	 */
-	private static void get5_sports() {
-		Node xml = common.getXmlData("lotto/data_sports.xml");
+    /**
+     * 生成5注
+     *
+     * @param sameSize 最大相同数
+     */
+    private static void get5_sports() {
+        Node xml = common.getXmlData("lotto/data_sports.xml");
 
-		String[] oneSports = getOneSports();
+        String[] oneSports = getOneSports();
 
-		for (int i = 0; i < 20; i++) {
-			cal = 0;
-			if (i % 5 == 0) {
-				System.out.println();
-			}
-			do {
-				cal++;
-				oneSports = getOneSports();
-			} while (compare_sports(xml, oneSports));
-		}
-	}
+        for (int i = 0; i < 20; i++) {
+            cal = 0;
+            if (i % 5 == 0) {
+                System.out.println();
+            }
+            do {
+                cal++;
+                oneSports = getOneSports();
+            } while (compare_sports(xml, oneSports));
+        }
+    }
 
-	/**
-	 * 生成随机一注_sports
-	 * 
-	 * @return
-	 */
-	private static String[] getOneSports() {
-		// red 5/35
-		String[] reds = common.randomSequence(35, 5);
-		// blue 2/12
-		String[] bule = common.randomSequence(12, 2);
+    /**
+     * 生成随机一注_sports
+     *
+     * @return
+     */
+    private static String[] getOneSports() {
+        // red 5/35
+        String[] reds = common.randomSequence(35, 5);
+        // blue 2/12
+        String[] bule = common.randomSequence(12, 2);
 
-		String[] Sports = new String[7];
+        String[] Sports = new String[7];
 
-		for (int i = 0; i < Sports.length; i++) {
+        for (int i = 0; i < Sports.length; i++) {
 
-			if (i < 5) {
-				Sports[i] = reds[i];
-			} else {
-				Sports[i] = bule[i - 5];
-			}
-		}
+            if (i < 5) {
+                Sports[i] = reds[i];
+            } else {
+                Sports[i] = bule[i - 5];
+            }
+        }
 
-		return Sports;
-	}
+        return Sports;
+    }
 
-	/**
-	 * 比较历史数据_sports
-	 * 
-	 * @param xml
-	 *            历史数据 XML格式
-	 * @param oneSports
-	 *            随机一注
-	 * @param sameSize
-	 *            最大相同数
-	 * @return
-	 */
-	private static boolean compare_sports(Node xml, String[] oneSports) {
+    /**
+     * 比较历史数据_sports
+     *
+     * @param xml       历史数据 XML格式
+     * @param oneSports 随机一注
+     * @param sameSize  最大相同数
+     * @return
+     */
+    private static boolean compare_sports(Node xml, String[] oneSports) {
 
-		redSame = new int[6];
-		blueSame = new int[3];
-		allSame = new int[8];
+        redSame = new int[6];
+        blueSame = new int[3];
+        allSame = new int[8];
 
-		List<String> listRed = new ArrayList<String>();
-		List<String> listBlue = new ArrayList<String>();
+        List<String> listRed = new ArrayList<String>();
+        List<String> listBlue = new ArrayList<String>();
 
-		for (int i = 0; i < oneSports.length - 2; i++) {
-			listRed.add(oneSports[i]);
-		}
+        for (int i = 0; i < oneSports.length - 2; i++) {
+            listRed.add(oneSports[i]);
+        }
 
-		listBlue.add(oneSports[5]);
-		listBlue.add(oneSports[6]);
+        listBlue.add(oneSports[5]);
+        listBlue.add(oneSports[6]);
 
-		// 遍历
-		int size = xml.children().size();
-		for (int i = 0; i < size; i++) {
+        // 遍历
+        int size = xml.children().size();
+        for (int i = 0; i < size; i++) {
 
-			Node oneLotto = (Node) xml.children().get(i);
-			int redFlag = 0, blueFlag = 0;
+            Node oneLotto = (Node) xml.children().get(i);
+            int redFlag = 0, blueFlag = 0;
 
-			String red1 = (String) ((Node) oneLotto.children().get(0)).attribute("red");
-			String red2 = (String) ((Node) oneLotto.children().get(1)).attribute("red");
-			String red3 = (String) ((Node) oneLotto.children().get(2)).attribute("red");
-			String red4 = (String) ((Node) oneLotto.children().get(3)).attribute("red");
-			String red5 = (String) ((Node) oneLotto.children().get(4)).attribute("red");
-			String blue1 = (String) ((Node) oneLotto.children().get(5)).attribute("blue");
-			String blue2 = (String) ((Node) oneLotto.children().get(6)).attribute("blue");
+            String red1 = (String) ((Node) oneLotto.children().get(0)).attribute("red");
+            String red2 = (String) ((Node) oneLotto.children().get(1)).attribute("red");
+            String red3 = (String) ((Node) oneLotto.children().get(2)).attribute("red");
+            String red4 = (String) ((Node) oneLotto.children().get(3)).attribute("red");
+            String red5 = (String) ((Node) oneLotto.children().get(4)).attribute("red");
+            String blue1 = (String) ((Node) oneLotto.children().get(5)).attribute("blue");
+            String blue2 = (String) ((Node) oneLotto.children().get(6)).attribute("blue");
 
-			redFlag = common.getSameSizeRed(listRed, redFlag, red1, red2, red3, red4, red5);
-			blueFlag = common.getSameSizeBlue(listBlue, blueFlag, blue1, blue2);
+            redFlag = common.getSameSizeRed(listRed, redFlag, red1, red2, red3, red4, red5);
+            blueFlag = common.getSameSizeBlue(listBlue, blueFlag, blue1, blue2);
 
-			redSame[redFlag] += 1;
-			blueSame[blueFlag] += 1;
-			allSame[redFlag + blueFlag] += 1;
+            redSame[redFlag] += 1;
+            blueSame[blueFlag] += 1;
+            allSame[redFlag + blueFlag] += 1;
 
-		}
+        }
 
-		if (redSame[0] < 554 || redSame[0] > 817)
-			return true;
-		if (redSame[1] < 608 || redSame[1] > 764)
-			return true;
-		if (redSame[2] < 167 || redSame[2] > 255)
-			return true;
-		if (redSame[3] < 18 || redSame[3] > 32)
-			return true;
-		if (redSame[4] > 2)
-			return true;
-		if (redSame[5] > 0)
-			return true;
+        if (redSame[0] < 554 || redSame[0] > 817)
+            return true;
+        if (redSame[1] < 608 || redSame[1] > 764)
+            return true;
+        if (redSame[2] < 167 || redSame[2] > 255)
+            return true;
+        if (redSame[3] < 18 || redSame[3] > 32)
+            return true;
+        if (redSame[4] > 2)
+            return true;
+        if (redSame[5] > 0)
+            return true;
 
-		if (blueSame[0] < 1061 || blueSame[0] > 1190)
-			return true;
-		if (blueSame[1] < 450 || blueSame[1] > 544)
-			return true;
-		if (blueSame[2] < 20 || blueSame[2] > 36)
-			return true;
+        if (blueSame[0] < 1061 || blueSame[0] > 1190)
+            return true;
+        if (blueSame[1] < 450 || blueSame[1] > 544)
+            return true;
+        if (blueSame[2] < 20 || blueSame[2] > 36)
+            return true;
 
-		if (allSame[0] < 417 || allSame[0] > 565)
-			return true;
-		if (allSame[1] < 674 || allSame[1] > 738)
-			return true;
-		if (allSame[2] < 336 || allSame[2] > 400)
-			return true;
-		if (allSame[3] < 60 || allSame[3] > 136)
-			return true;
-		if (allSame[4] < 6 || allSame[4] > 18)
-			return true;
-		if (allSame[5] > 1)
-			return true;
-		if (allSame[6] > 0)
-			return true;
-		if (allSame[7] > 0)
-			return true;
+        if (allSame[0] < 417 || allSame[0] > 565)
+            return true;
+        if (allSame[1] < 674 || allSame[1] > 738)
+            return true;
+        if (allSame[2] < 336 || allSame[2] > 400)
+            return true;
+        if (allSame[3] < 60 || allSame[3] > 136)
+            return true;
+        if (allSame[4] < 6 || allSame[4] > 18)
+            return true;
+        if (allSame[5] > 1)
+            return true;
+        if (allSame[6] > 0)
+            return true;
+        if (allSame[7] > 0)
+            return true;
 
-		printLotto_sports(oneSports);
-		return false;
-	}
+        printLotto_sports(oneSports);
+        return false;
+    }
 
-	/**
-	 * 打印生成的数据
-	 *
-	 * @param oneSports
-	 */
-	private static void printLotto_sports(String[] oneSports) {
-		for (int i = 0; i < oneSports.length; i++) {
-			if (i == 4)
-				System.out.print(oneSports[i] + "   ");
-			else
-				System.out.print(oneSports[i] + " ");
+    /**
+     * 打印生成的数据
+     *
+     * @param oneSports
+     */
+    private static void printLotto_sports(String[] oneSports) {
+        for (int i = 0; i < oneSports.length; i++) {
+            if (i == 4)
+                System.out.print(oneSports[i] + "   ");
+            else
+                System.out.print(oneSports[i] + " ");
 
-		}
-		System.out.print("\t\t" + cal);
-		System.out.println();
-	}
+        }
+        System.out.print("\t\t" + cal);
+        System.out.println();
+    }
 
 }
